a8bd0ced9cab2cd060e0446b5eee7acc433afd1a
Kry9toN <dhimasbagusprayoga@gmail.com>
2019-08-03 19:22:51 +0700

    test

--- index.php
+++ index.php
@@ -101,87 +101,12 @@ while($r=mysqli_fetch_array($query)){
 			</div>
 		</div>
 	</div>
-
-		<!-- GALLERY -->
-		<div class="row content" id="galeri">
-			<div class="col-md-12 primary text-center zero-panel">
-				<div class="col-md-8 zero-panel-content">
-					<h1> GALERI KEGIATAN OSIS </h1>
-					<p>
-						Inilah beberapa kegiatan OSIS yang terdokumentasikan. Silahkan klik untuk tampilan secara rinci.
-					</p>
-				</div>
-				<div class="col-md-4 col-sm-6 gallery">
-					<a class="gallery-link" href="#">
-						<div class="caption">
-							<div class="caption-content danger">
-								<span>Sanlat 2017</span>
-							</div>
-						</div>
-						<img src="gambar/galeri5.jpg" class="img-responsive" alt>
-					</a>
-				</div>
-				<div class="col-md-4 col-sm-6 gallery">
-					<a class="gallery-link" href="#">
-						<div class="caption">
-							<div class="caption-content danger">
-								<span>Tahun Ajaran Baru</span>
-							</div>
-						</div>
-						<img src="gambar/galeri1.jpg" class="img-responsive" alt>
-					</a>
-				</div>
-				<div class="col-md-4 col-sm-6 gallery">
-					<a class="gallery-link" href="#">
-						<div class="caption">
-							<div class="caption-content danger">
-								<span>Ujian Tengah Semester</span>
-							</div>
-						</div>
-						<img src="gambar/galeri3.jpg" class="img-responsive" alt>
-					</a>
-				</div>
-				<div class="col-md-4 col-sm-6 gallery">
-					<a class="gallery-link" href="#">
-						<div class="caption">
-							<div class="caption-content danger">
-								<span>Workshop Depicta 2016</span>
-							</div>
-						</div>
-						<img src="gambar/galeri2.jpg" class="img-responsive" alt>
-					</a>
-				</div>
-				<div class="col-md-4 col-sm-6 gallery">
-					<a class="gallery-link" href="#">
-						<div class="caption">
-							<div class="caption-content danger">
-								<span>Sertijab 2014</span>
-							</div>
-						</div>
-						<img src="gambar/galeri4.jpg" class="img-responsive" alt>
-					</a>
-				</div>
-				<div class="col-md-4 col-sm-6 gallery">
-					<a class="gallery-link" href="#">
-						<div class="caption">
-							<div class="caption-content danger">
-								<span>Nuzulul Qur'an</span>
-							</div>
-						</div>
-						<img src="gambar/galeri6.jpg" class="img-responsive" alt>
-					</a>
-				</div>
-
-				<div class="clear"></div>
-			</div>
-		</div>
-		<!-- END GALLERY -->
 		
 		<!-- LOGIN -->
-		<div class="row content" id="login">
-			<div class="col-lg-12 danger text-center zero-panel">
-				<div class="col-lg-12 zero-panel-content">
-				<div class="card-body">
+	<div class="row content" id="login">
+      <div class="container text-center mt-2">
+        <div class="card text-white bg-dark mb-3">
+          <div class="card-body">
 					<h1> LOGIN sebagai .... </h1>
 					<p>Silahkan login untuk memberikan suara. Login sebagai ...</p>
 				</div>
@@ -214,7 +139,10 @@ while($r=mysqli_fetch_array($query)){
 		<!-- END LOGIN -->
 		<!-- END CONTENT -->
 
-		<footer class="primary text-center">
+<footer class="bg-secondary text-white">
+  <div class="container">
+      <div class="card text-white bg-dark mb-3">
+          <div class="card-body">
 			<div class="row">
 				<div class="col-md-4">
 					<h3>PILKASIS</h3>
@@ -227,67 +155,16 @@ while($r=mysqli_fetch_array($query)){
 						<a href="http://github.com/KryPtoN" target="_blank" class="btn btn-xs btn-outline"><i class="fab fa-github fa-2x"></i></a>
 						<a href="http://google.com/muhidins" target="_blank" class="btn btn-xs btn-outline"><i class="fa fa-google-plus fa-2x"></i></a>
 						<a href="http://twitter.com/muhidins" target="_blank" class="btn btn-xs btn-outline"><i class="fa fa-twitter fa-2x"></i></a>
-				</div>
-			</div>
-		</footer>
-
-		<!-- MODAL -->
-		<div class="zero-modal">
-			<div class="close-modal">&times;</div>
-			<div class="modal-content">
-				<div class="modal-body">
-					<div class="col-md-6 contain">
-						<h1 class="title">PROJECT TITLE</h1>
-						<div class="box-modal">
-							<p>Mentafakuri Ayat-ayat di Alam Semesta, Korelasi Rumus Fisika dan Ayat Al-Qur'an" itulah Materi Sanlat Selasa, 13 Juni 2017 yang diikuti oleh seluruh siswa/i SMA PRO Kelas 10 dan 11, pemateri disampaikan oleh Bapak Ryksa Raharja, M.Pd.</p>
-						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- END MODAL -->
+	</div/
+</footer>
 
-		<script type="text/javascript" src="assets/js/jquery.easypiechart.min.js"></script>
-		<script>
-			var img = ['gambar/depicta2016.jpg', 'gambar/rohis.jpg','gambar/sertijab.jpg'];
-			$(".cover").zeroSlide(img, 5000);
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-			// ANIMATE
-			$("#title-about").zeroAnimate('fadein');
-			$("#img-about1").zeroAnimate('slideFromLeft');
-			$("#img-about2").zeroAnimate('slideFromRight');
-			$("#about-service .col-md-4").each(function() {
-				$(this).zeroAnimate('fadein');
-			})
-			$(".gallery .gallery-link").each(function() {
-				$(this).zeroAnimate('slideFromLeft');
-			});
-			$("#box-contact").zeroAnimate('slideFromLeft');
-			$("#box-map").zeroAnimate('fadein');
-			$("#chart-skill li").each(function() {
-				$(this).zeroAnimate('slideFromRight');
-			})
-			$("#our-process .media-border").each(function() {
-				$(this).zeroAnimate("slideFromLeft");
-			});
-			$(".gallery-author").zeroAnimate('slideFromRight');
-			// ENABLE LOADING ANIMATE
-			$.zeroLoading("circle-simple");
-
-			// SKILL CHART
-			var oldie = /msie\s*(8|7|6)/.test(navigator.userAgent.toLowerCase());
-			$('.easy-pie-chart.percentage').each(function(){
-				$(this).easyPieChart({
-					barColor: $(this).data('color'),
-					trackColor: '#DDD',
-					scaleColor: false,
-					lineCap: 'butt',
-					lineWidth: 30,
-					animate: oldie ? false : 1000,
-					size:264
-				}).css('color', $(this).data('color'));
-			});
-
-		</script>
 	</body>
 </html>
