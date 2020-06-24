5bc194eccfab444f4d98d6f6af476720f52ff5d4
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-06-24 08:24:02 +0000

    move to assets

--- index.php
+++ index.php
@@ -101,6 +101,6 @@ while($r=mysqli_fetch_array($query)){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="/js/style.js"></script>
+    <script src="/assets/js/style.js"></script>
   </body>
 </html>
